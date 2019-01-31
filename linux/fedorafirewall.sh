035f2bea32071f2c52212f4991bbe4c8e57c7e73
Adam I <15039983+TheOtherUnknown@users.noreply.github.com>
2019-01-31 15:13:56 -0500

    Finish firewall rules

--- linux/fedorafirewall.sh
+++ linux/fedorafirewall.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
- Not COMPLETE!
 systemctl mask firewalld
 iptables -F
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 25 -j ACCEPT
+iptables -A INPUT -p tcp --dport 110 -j ACCEPT
 iptables -I INPUT 1 -i lo -j ACCEPT # loopback
 sysctl -w net.ipv6.conf.all.disable_ipv6=1 # Kill IPv6
 sysctl -w net.ipv6.conf.default.disable_ipv6=1
