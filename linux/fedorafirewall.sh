c0715655bdd12813a22107b5615ba444d7461e2d
Adam I <15039983+TheOtherUnknown@users.noreply.github.com>
2019-03-10 12:19:53 -0400

    Add comments for PPT

--- linux/fedorafirewall.sh
+++ linux/fedorafirewall.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 systemctl stop firewalld
-systemctl mask firewalld
-iptables -F
-iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-iptables -A INPUT -p tcp --dport 25 -j ACCEPT
-iptables -A INPUT -p tcp --dport 110 -j ACCEPT
-iptables -I INPUT 1 -i lo -j ACCEPT # loopback
+systemctl mask firewalld # Disable Fedora's preinstalled firewall
+iptables -F # Flush all rules
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT # Allow established connections
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT # Allow incoming traffic to TCP port 80 (0.0.0.0:80)
+iptables -A INPUT -p tcp --dport 25 -j ACCEPT # Allow incoming traffic to TCP port 25
+iptables -A INPUT -p tcp --dport 110 -j ACCEPT # Allow incoming traffic to TCP port 110
+iptables -I INPUT 1 -i lo -j ACCEPT # Allow traffic over the loopback interface (localhost)
 sysctl -w net.ipv6.conf.all.disable_ipv6=1 # Kill IPv6
 sysctl -w net.ipv6.conf.default.disable_ipv6=1
 # Keep at end!
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
+iptables -P INPUT DROP # Set default INPUT policy to DROP (Block)
+iptables -P FORWARD DROP # Set default FORWARD policy to DROP (Block)
